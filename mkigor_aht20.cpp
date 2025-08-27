4435a777472a809b94d7a182839b854d6f40f26a
Igor Mkprog <mkprogigor@gmail.com>
2025-08-27 12:47:34 +0200

    4 commit

--- mkigor_aht20.cpp
+++ mkigor_aht20.cpp
@@ -5,7 +5,19 @@ by Igor Mkprog, mkprogigor@gmail.com
 use examples:
 https://github.com/peff74/ESP_AHT20_BMP280
 
-V1.0 from 18.06.2025
+V1.1 from 30.08.2025
+
+Glossary, abbreviations used in the module, prefix and suffix:
+gv_*    -   Global Variable;
+lv_*    -   Local Variable (live inside statement);
+cl_*    -   CLass;
+cd_*    -   Class Definishion;
+cgv_*   -   Class public (Global) member (Variable);
+clv_*   -   Class private (Local) member (Variable);
+cgf_*   -   Class public (Global) metod (Function), not need, no usefull, becouse we see parenthesis => ();
+clf_*   -   Class private (Local) metod (Function);
+
+*_stru  -   [or *_stru_t] suffix, as usual, point the type.
 ************************************************************************************/
 #include <mkigor_aht20.h>
 
